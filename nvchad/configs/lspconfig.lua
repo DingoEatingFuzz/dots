544a1175f6c0d073a638720ee8e7eed7e8db5b34
Michael Lange <dingoeatingfuzz@gmail.com>
2024-10-19 18:31:28 -0700

    Move to ts_ls

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "ember", "lua_ls", "rust_analyzer", "gopls", "zls", "eslint" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "ember", "lua_ls", "rust_analyzer", "gopls", "zls", "eslint" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
