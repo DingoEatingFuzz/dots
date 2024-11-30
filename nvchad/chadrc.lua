f6270c04c1bf96d49c79144675d083ecb2cff47d
Michael Lange <dingoeatingfuzz@gmail.com>
2024-11-30 14:14:29 -0800

    Clean up and reform to current NvChad main

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -1,20 +1,22 @@
 ---@type ChadrcConfig
 local M = {}
 
--- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
-
 M.ui = {
-  theme = "oceanic-next",
-  theme_toggle = { "oceanic-next", "one_light" },
+	theme = "oceanic-next",
+	theme_toggle = { "oceanic-next", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = {
+		Comment = { italic = true },
+	},
+
+	hl_add = {
+		NvimTreeOpenedFolderName = { fg = "green", bold = true },
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
